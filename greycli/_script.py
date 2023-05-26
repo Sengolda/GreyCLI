852fa1ea300806c3f60d8bd65ce747412be9b242
Sengolda <senushkabatawala@gmail.com>
2023-05-26 16:53:32 +0530

    Support greybot rewrite

---
+++ greycli/_script.py
@@ -0,0 +1,60 @@
+import click
+import os
+import subprocess
+import sys
+import functools
+
+run_cmd = functools.partial(subprocess.run, shell=True, capture_output=True, text=True)
+
+to_print = """
+    ____                 ____ _     ___ 
+    / ___|_ __ ___ _   _ / ___| |   |_ _|
+    | |  _| '__/ _ \ | | | |   | |    | | 
+    | |_| | | |  __/ |_| | |___| |___ | | 
+    \____|_|  \___|\__, |\____|_____|___|
+                    |___/ 
+"""
+
+config_template = """
+TOKEN={}
+OWNER_IDS=[{}]
+"""
+
+
+@click.command()
+@click.option("--name", help="Name of your project", default=None)
+@click.option(
+    "--force",
+    is_flag=True,
+    help="Clear the directory and make the project",
+    default=False,
+)
+def main(name, force):
+    if os.path.isdir(name) and not force:
+        click.echo("That folder already exists if you want to delete its contents do the command with the --force flag")
+        return
+    if os.path.isdir(name) and force:
+        run_cmd(f"rm -rf {name}")
+    click.echo(to_print, color="cyan")
+    token = input("Token:")
+    user_id = input("\nYour discord user ID:")
+    if sys.platform not in ("win32", "cygwin", "cli"):
+        run_cmd(
+            f"git clone https://github.com/Sengolda/greybot.git && mv greybot {name}"
+        )
+        run_cmd(f"cd {name} && rm -f config.py.example")
+        config = config_template.format(token, user_id)
+        run_cmd(f"touch {name}/config.py")
+        with open(f"{name}/config.py", "w") as f:
+            f.write(config)
+
+        click.echo("[+] Added env file.")
+        click.echo(
+            f"[+] Success, your template is ready\nto start it just do `cd {name} && sh startbot.sh`",
+        )
+    else:
+        print("Sorry, your system is not supported by GreyCLI.")
+
+
+if __name__ == "__main__":
+    main()
--- readme.md
+++
@@ -1,8 +0,0 @@
-# GreyCLI
-- A CLI that helps you setup the [greybot template](https://github.com/Sengolda/greybot.git)
-
-### How to get started.
-- Clone this repo and cd to it and run `python3 main.py`, this will gen your project.
-
-### NOTE:
-- You need to run `pip install termcolor` as it is used for some things of this cli.
